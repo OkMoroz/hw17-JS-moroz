38467ca94a6010d9559cb81ce0b328fdde78daa2
Oksana Moroz <k.moroz.mail@gmail.com>
2023-06-20 22:04:31 +0300

    animation and css

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
-$("#btn").on("click", function () {
-  $("#modalWindow").dialog();
-  $("#modalWindow").css("display", "block");
+$(document).ready(function () {
+  $("#btn").on("click", function () {
+    $("#modalWindow").dialog({
+      show: {
+        effect: "fade",
+        duration: 600,
+      },
+      hide: {
+        effect: "fade",
+        duration: 600,
+      },
+    });
+
+    $("#btn").hide();
+  });
+
+  $("#modalWindow").on("dialogclose", function () {
+    $("#btn").show();
+  });
+
+  $("#btn").hover(
+    function () {
+      $(this).css({
+        "background-color": "rgba(248, 238, 238, 0.662)",
+        color: "rgb(248, 238, 238)",
+      });
+    },
+    function () {
+      $(this).css({
+        "background-color": "",
+        color: "",
+      });
+    }
+  );
+
+  $("#modalWindow").hover(
+    function () {
+      $(this).css({
+        "background-color": "#2421328f",
+        color: "rgb(248, 238, 238)",
+      });
+    },
+    function () {
+      $(this).css({
+        "background-color": "",
+        color: "",
+      });
+    }
+  );
 });
